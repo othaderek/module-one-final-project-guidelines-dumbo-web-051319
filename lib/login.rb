404546e63a275c7bd637c227367bb0ad8059232a
othaderek <otha.hernandez@gmail.com>
2019-05-29 15:49:40 -0400

    Moved login methods to own file.

--- lib/login.rb
+++ lib/login.rb
@@ -1,10 +1,30 @@
-# require "pry"
-# def login
-#   prompt = TTY::Prompt.new
-#   user_name = prompt.ask('What is your name?')
-#   binding.pry
-#   if user_name == User.all.find
-#     puts "Hello #{user_name}"
-#   end
-#
-# end
+def login
+
+  r = []
+  font = TTY::Font.new(:standard)
+  prompt = TTY::Prompt.new
+  pastel = Pastel.new
+  puts pastel.green(font.write("LOGIN"))
+  user_name = prompt.ask('username:')
+  User.all.collect{|u| r << u if u.name == user_name}
+  p = r.pop
+  if p.name == user_name
+    ruby_clear
+    puts pastel.green(font.write("PASSWORD"))
+    pw = prompt.mask("password:")
+    if pw == p.password
+      puts "Success!"
+      puts "Welcome #{p.name}"
+    else
+      list_input_prompt
+    end
+  end
+  # input_selector(p)
+end
+
+
+def list_input_prompt
+  prompt = TTY::Prompt.new
+  input = prompt.select("Welcome!", ["Login", "Create User"])
+  input_selector(input)
+end
--- lib/main_menu.rb
+++ lib/main_menu.rb
@@ -39,48 +39,6 @@ def invalid_input_prompt
   puts ""
 end
 
-# def list_input_prompt
-#   puts ""
-#   puts "List characters:  1".center(65)
-#   puts "List spells:      2".center(65)
-#   puts "Create character: 3".center(65)
-#   puts "Delete character: 4".center(65)
-#   puts "Select Character: 5".center(65)
-#   puts "Exit program:     q".center(65)
-#   puts ""
-# end
-
-def login
-
-  r = []
-  font = TTY::Font.new(:standard)
-  prompt = TTY::Prompt.new
-  pastel = Pastel.new
-  puts pastel.green(font.write("LOGIN"))
-  user_name = prompt.ask('username:')
-  User.all.collect{|u| r << u if u.name == user_name}
-  p = r.pop
-  if p.name == user_name
-    ruby_clear
-    puts pastel.green(font.write("PASSWORD"))
-    pw = prompt.mask("password:")
-    if pw == p.password
-      puts "Success!"
-      puts "Welcome #{p.name}"
-    else
-      list_input_prompt
-    end
-  end
-  # input_selector(p)
-end
-
-
-def list_input_prompt
-  prompt = TTY::Prompt.new
-  input = prompt.select("Welcome!", ["Login", "Create User"])
-  input_selector(input)
-end
-
 
 
 def input_selector(input)
@@ -114,36 +72,6 @@ def input_selector(input)
 
 end
 
-# def input_selector(input)
-#
-#   case input
-#   when "1"
-#     ruby_clear
-#     list_characters_prompt
-#   when "2"
-#     ruby_clear
-#     list_spells_prompt
-#   when "3"
-#     ruby_clear
-#     create_character_prompt
-#     ruby_clear
-#   when "4"
-#     ruby_clear
-#     delete_character_prompt
-#   when "5"
-#     ruby_clear
-#     select_character_prompt
-#     character_paperdoll
-#     ruby_clear
-#   when "q"
-#     ruby_clear
-#     exit_program_prompt
-#   else
-#     ruby_clear
-#     invalid_input_prompt
-#   end
-#
-# end
 
 def menu_loop
   input = ""
