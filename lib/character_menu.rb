8a269064b17176787d7a528e8fd96198b2e1aa0c
Joe Jamsky <cba1067950@yahoo.com>
2019-05-29 17:49:01 -0400

    character menu

--- lib/character_menu.rb
+++ lib/character_menu.rb
@@ -1,42 +1,44 @@
-def character_menu_loop
+def character_list_input_prompt
+  prompt1 = TTY::Prompt.new
+  # prompt1.ask("Select     ")
+  var = prompt1.select("CHOOSE NOW PLEASE NOW", ["Spells".center(60), "Edit".center(60), "Delete".center(60), "Assign Spell".center(60), "Main Menu".center(60)])
+end
 
-    input = ""
-    while input != "q"
-      splash
-      list_input_prompt(user1)
-      input = gets.chomp
-      input_selector(input)
-    end
+def character_menu_loop(character)
+
+  input = ""
+  while input != "Main Menu"
+    input = character_list_input_prompt.strip
+    # input = gets.chomp
+    character_input_selector(input, character)
+  end
 
 end
 
+def character_input_selector(input, character)
 
-def character_input_selector(input, len)
-
-  intput = input.to_i
-
-  if intput > 0 && intput < len -1
-    case intput
-    when Fixnum
-      ruby_clear
-      puts "Is number"
+  case input
+  when "Spells"
+    # user1.characters[0].spells
+    puts ""
+    character.spells.each do |spell|
+      puts "Spell Name: #{spell.name}"
+      puts "Spell Description: #{spell.description}"
     end
+    puts ""
+    puts ""
+    # binding.pry
+  when "Edit"
+    ruby_clear
+
+  when "Delete"
+    character.delete
+  when "Add Spell To Book"
+    ruby_clear
+    # list spells to assign
   else
-    case input
-    when "C"
-      ruby_clear
-      puts "create character"
-    when "S"
-      ruby_clear
-      create_character_prompt
-      ruby_clear
-    when "q"
-      ruby_clear
-      exit_program_prompt
-    else
-      ruby_clear
-      invalid_input_prompt
-    end
+    ruby_clear
+    invalid_input_prompt
   end
-  
+
 end
