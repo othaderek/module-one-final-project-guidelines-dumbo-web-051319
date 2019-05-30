07dea4ff6ee4a06f4d446b9cd96ea3f212f9eb19
Joe Jamsky <cba1067950@yahoo.com>
2019-05-30 12:44:18 -0400

    ALLLL LINKED

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,6 @@
 require_relative '../config/environment'
-list_input_prompt
-
+user = list_input_prompt
 # spinner
-user1 = User.all[0]
-main_menu_loop(user1)
+main_menu_loop(user)
 
 puts "Goodbye"
